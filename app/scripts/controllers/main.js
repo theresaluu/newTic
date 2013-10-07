50859dcf321ff19e287514c2c88647ea0bcc40fe
Theresa Luu <theresa.luu@gmail.com>
2013-10-07 11:05:41 -0700

    installed firebase and angularfire- doesn't render

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,23 +1,42 @@
 'use strict';
 
-angular.module('newTicApp')
-  .controller('TicCtrl', function($scope){
-    $scope.turn = 1;
+var newTic = angular.module('newTicApp', ['firebase'])
+  .controller('TicCtrl', [
+    '$scope', 
+    'angularFire',
+    '$cookies',
+     function($scope, angularFire, $cookies){
 
-    $scope.play= function(cell){
-      if($scope.turn % 2 == 0){
-        cell.value="X";
-      }
-      else{
-        cell.value="O";
-      }
-      $scope.turn++;
-    }; 
+      var myRootRef = new Firebase('https://newtic.firebaseio.com/');
+  
+      $scope.turn = 1;
 
-    $scope.boxes = [[{value: ""}, {value: ""}, {value: ""}], 
-    [{value: ""}, {value: ""}, {value: ""}],
-    [{value: ""}, {value: ""}, {value: ""}]];
-  });
+      $scope.boxes = [[{value: ""}, {value: ""}, {value: ""}], 
+      [{value: ""}, {value: ""}, {value: ""}],
+      [{value: ""}, {value: ""}, {value: ""}]];
+
+      angularFire(myRootRef, $scope, "boxes");
+
+      $scope.myRootRef.set($scope.boxes);
+
+      $scope.play= function(cell){
+        if($scope.turn % 2 == 0){
+          cell.value="X";
+        }
+        else{
+          cell.value="O";
+        }
+        $scope.turn++;
+      }; 
+
+      // $scope.win=function(cell){
+
+      // };
+
+      // $scope.clear=function(cell){
+
+      // };
+    }]);
 
 // document.getElementsByClassName("tile").onclick=function(){
 
